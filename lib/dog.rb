bb4ddcbad4201c7403569ddb53d64054a5017296
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-21 00:27:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
-class Dog 
-  def name=(dog_name)
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
 end
-
-def name 
-  this_dog_name
-end
-end
+ 
 lassie = Dog.new
 lassie.name = "Lassie"
  
